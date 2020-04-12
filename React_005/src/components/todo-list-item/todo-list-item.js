a27cd9d820307e567d540539b469af6e4385cd1e
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-12 19:53:58 +0400

    OnClick react 3 ways

---
+++ React_005/src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+export default class TodoListItem extends Component{
+
+    onLabelCLick=()=> {
+        console.log(this.props.label)
+    };
+
+    render() {
+        const {label,important=false}=this.props;
+
+        const style = {
+            color: important ? 'steelblue' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        };
+
+        return (
+            <span className="todo-list-item">
+      <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.onLabelCLick}>
+          {label}
+      </span>
+
+      <button type="button"
+              className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+
+      <button type="button"
+              className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </span>
+        );
+    }
+}
