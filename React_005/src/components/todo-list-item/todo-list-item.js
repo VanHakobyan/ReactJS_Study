ac5c9cf0a68e0fe698ca61cb6c46e4875dfaa524
vanhakobyan1996 <vanhakobyan1996@gmail.com>
2020-04-12 20:15:09 +0400

    state, setState

--- React_005/src/components/todo-list-item/todo-list-item.js
+++ React_005/src/components/todo-list-item/todo-list-item.js
@@ -1,37 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './todo-list-item.css';
-export default class TodoListItem extends Component{
 
-    onLabelCLick=()=> {
-        console.log(this.props.label)
+export default class TodoListItem extends Component {
+
+    state = {
+        done: false,
+        important: false
+    };
+
+
+    onMarkImportant = () => {
+        this.setState({
+            important: true
+        });
+    };
+    onLabelCLick = () => {
+        this.setState({
+            done: true
+        });
     };
 
     render() {
-        const {label,important=false}=this.props;
+        const {label} = this.props;
+        const {done, important} = this.state;
 
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        let classNames = 'todo-list-item';
+        if (important) {
+            classNames += ' important';
+        }
 
-        return (
-            <span className="todo-list-item">
+        if (done) {
+            classNames += ' done';
+        }
+
+        return (<span className={classNames}>
       <span
           className="todo-list-item-label"
-          style={style}
           onClick={this.onLabelCLick}>
           {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={this.onMarkImportant}>
+
+        <i className="fa fa-exclamation"/>
       </button>
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
+        <i className="fa fa-trash-o"/>
       </button>
     </span>
         );
